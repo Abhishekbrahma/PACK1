27e4e4b2b8faee4aedf8bd5efc0d87ccafb037d4
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-23 18:02:07 +0530

    add two no

---
+++ actions/sum.py
@@ -0,0 +1,9 @@
+import sys
+from st2common.runners.base_action import Action
+
+class MyAction(Action):
+
+    def run(self, a,b):
+        c = int(a)+int(b)
+        print(c)
+        return(True,c)
